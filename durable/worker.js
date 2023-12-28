696321b80a7edba0f3f1f7844c19fede6470b16e
Luke Birdeau <luke.birdeau@gmail.com>
2023-12-27 23:00:50 -0800

    update packages

--- durable/worker.js
+++ durable/worker.js
@@ -3,17 +3,16 @@ const Redis = require('ioredis');
 const helloworldExample = require('./helloworld/workflows');
 
 async function initDurableWorker() {
-  const connection = await Durable.NativeConnection.connect({
-    class: Redis,
-    options: {
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT,
-      // password: config.REDIS_PASSWORD,
-      // db: config.REDIS_DATABASE,
-    },
-  });
   const worker = await Durable.Worker.create({
-    connection,
+    connection: {
+      class: Redis,
+      options: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        // password: config.REDIS_PASSWORD,
+        // db: config.REDIS_DATABASE,
+      },
+    },
     namespace: 'helloworldExample',
     taskQueue: 'helloworld',
     workflow: helloworldExample,
