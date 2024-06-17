8ba3d049f6e0a15f9795b30d1c9f4ee09987a8e1
Luke Birdeau <luke.birdeau@gmail.com>
2024-06-17 09:16:11 -0700

    sleepFor example

--- demo/durable/workflows.js
+++ demo/durable/workflows.js
@@ -4,13 +4,17 @@ const activities = require('./activities');
 const { greet, saludar } = Durable.workflow.proxyActivities({
   activities,
   retryPolicy: {
-    maximumAttempts: 1000, //I hope not!
+    maximumAttempts: 1_000, //I hope not!
     maximumInterval: '5 seconds',
     backoffCoefficient: 2,
   }
 });
 
 async function example(name, lang) {
+  //sleep to showcase replay/reentrance
+  await Durable.workflow.sleepFor('1 second');
+
+  //execute a proxied activity
   if (lang === 'es') {
     return await saludar(name);
   } else {
